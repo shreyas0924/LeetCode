035117b2b73aa63b2c72077afe34ee25dd42d6a3
Shreyas P <72003665+shreyas0924@users.noreply.github.com>
2023-06-02 21:46:20 +0530

    Time: 80 ms (18.11%), Space: 45.7 MB (38.56%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.ts
@@ -0,0 +1,19 @@
+type ReturnObj = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): ReturnObj {
+    let count: number = init
+    return {
+        increment: () => count += 1, decrement: () => count -= 1, reset: () => count=init 
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
