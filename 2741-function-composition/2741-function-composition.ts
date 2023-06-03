444a62e38d3d3e479e5e45885096c40b72dd0fe8
Shreyas P <72003665+shreyas0924@users.noreply.github.com>
2023-06-03 08:56:53 +0530

    Time: 81 ms (35.67%), Space: 46.2 MB (15.04%) - LeetHub

--- 2741-function-composition/2741-function-composition.ts
+++ 2741-function-composition/2741-function-composition.ts
@@ -1,10 +1,11 @@
-type F = (x: number) => number;
+// Method 1 : Using reduceRight method
+// type F = (x: number) => number;
 
-function compose(functions: F[]): F {
-	return function(x) {
-        return functions.reduceRight((acc, curr) => curr(acc), x )
-    }
-};
+// function compose(functions: F[]): F {
+// 	return function(x) {
+//         return functions.reduceRight((acc, curr) => curr(acc), x )
+//     }
+// };
 
 //Example of reduceRight in TS
 
@@ -17,3 +18,19 @@ function compose(functions: F[]): F {
 // console.log(sum); 
 
 // Output: 15 (5 + (4 + (3 + (2 + 1))))
+
+//Method 2 : Using normal for loops
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    return function (x) {
+        let value = x;
+
+        for (let i = functions.length - 1; i >= 0; i--) {
+            value = functions[i](value);
+        }
+
+        return value;
+    };
+}
+
