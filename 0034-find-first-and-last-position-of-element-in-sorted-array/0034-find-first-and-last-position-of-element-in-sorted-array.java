c05708b68d0679b29542a9174c54dfede487e73a
Shreyas P <72003665+shreyas0924@users.noreply.github.com>
2023-08-24 13:54:19 +0530

    Time: 1 ms (18.31%), Space: 44.3 MB (84.03%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,51 +1,81 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
            
-        int[] res = new int[2];
+
+        //two poitners
+        int arr[]=new int[2];
+        arr[0]=-1;
+        arr[1]=-1;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+            arr[0]=i;
+            break;
+            }
+        }
+        for(int j=nums.length-1;j>=0;j--)
+        {
+            if(nums[j]==target)
+            {
+            arr[1]=j;
+            break;
+            }
+        }
+        return arr;
         
-        res[0] = first(nums, target);
-        res[1] = second(nums,target);
+        
+        
+        //binary search
+//         int[] res = new int[2];
+        
+//         res[0] = first(nums, target);
+//         res[1] = second(nums,target);
+
        
-        return res;
+//         return res;
     }
     
-    public int first(int[] nums, int target){ //first half
-        int l = 0;
-        int h = nums.length - 1;
-        int ans = -1;
-        while(l <= h){
-            int m = (l + h)/2;
+//     public int first(int[] nums, int target){ //first half
+//         int l = 0;
+//         int h = nums.length - 1;
+//         int ans = -1;
+//         while(l <= h){
+//             int m = (l + h)/2;
             
-            if(nums[m] == target){
-                ans = m;
-                h = m-1;
-            }
-            else if(nums[m] > target){
-                h = m-1;
-            }
-            else l = m+1;
-        }
+//             if(nums[m] == target){
+//                 ans = m;
+//                 h = m-1;
+//             }
+//             else if(nums[m] > target){
+//                 h = m-1;
+//             }
+//             else l = m+1;
+//         }
         
-        return ans;
-    }
+//         return ans;
+//     }
     
-    public int second(int[] nums, int target){  //second half
-        int l = 0;
-        int h = nums.length - 1;
-        int ans = -1;
-        while(l <= h){
-            int m = (l + h)/2;
+//     public int second(int[] nums, int target){  //second half
+//         int l = 0;
+//         int h = nums.length - 1;
+//         int ans = -1;
+//         while(l <= h){
+//             int m = (l + h)/2;
             
-            if(nums[m] == target){
-                ans = m;
-                l = m+1;
-            }
-            else if(nums[m] > target){
-                h = m-1;
-            }
-            else l = m+1;
-        }
+//             if(nums[m] == target){
+//                 ans = m;
+//                 l = m+1;
+//             }
+//             else if(nums[m] > target){
+//                 h = m-1;
+//             }
+//             else l = m+1;
+//         }
         
-        return ans;
-    }
+//         return ans;
+//     }
+    
+    
+    
 }
