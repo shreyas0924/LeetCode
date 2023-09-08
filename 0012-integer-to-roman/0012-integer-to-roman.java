63638626922c5d84d91fa076b6ce014d4be48f33
Shreyas P <72003665+shreyas0924@users.noreply.github.com>
2023-09-08 08:02:57 +0530

    Time: 3 ms (98.54%), Space: 43.3 MB (59.46%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -5,13 +5,14 @@ class Solution {
         int[] romanValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
       
         StringBuilder romanBuilder = new StringBuilder();
-
-    for(int i = 0; i < romanValues.length; i++) {
-        while (num >= romanValues[i]) {
-            romanBuilder.append(romanSymbols[i]);
-            num -= romanValues[i];
+        
+        for(int i=0; i<romanValues.length ;i++){
+            while(num >= romanValues[i]){
+                romanBuilder.append(romanSymbols[i]);
+                num -= romanValues[i];
+                
+            }
+        }
+        return romanBuilder.toString();
     }
-}
-     return romanBuilder.toString();
-  }
 }
