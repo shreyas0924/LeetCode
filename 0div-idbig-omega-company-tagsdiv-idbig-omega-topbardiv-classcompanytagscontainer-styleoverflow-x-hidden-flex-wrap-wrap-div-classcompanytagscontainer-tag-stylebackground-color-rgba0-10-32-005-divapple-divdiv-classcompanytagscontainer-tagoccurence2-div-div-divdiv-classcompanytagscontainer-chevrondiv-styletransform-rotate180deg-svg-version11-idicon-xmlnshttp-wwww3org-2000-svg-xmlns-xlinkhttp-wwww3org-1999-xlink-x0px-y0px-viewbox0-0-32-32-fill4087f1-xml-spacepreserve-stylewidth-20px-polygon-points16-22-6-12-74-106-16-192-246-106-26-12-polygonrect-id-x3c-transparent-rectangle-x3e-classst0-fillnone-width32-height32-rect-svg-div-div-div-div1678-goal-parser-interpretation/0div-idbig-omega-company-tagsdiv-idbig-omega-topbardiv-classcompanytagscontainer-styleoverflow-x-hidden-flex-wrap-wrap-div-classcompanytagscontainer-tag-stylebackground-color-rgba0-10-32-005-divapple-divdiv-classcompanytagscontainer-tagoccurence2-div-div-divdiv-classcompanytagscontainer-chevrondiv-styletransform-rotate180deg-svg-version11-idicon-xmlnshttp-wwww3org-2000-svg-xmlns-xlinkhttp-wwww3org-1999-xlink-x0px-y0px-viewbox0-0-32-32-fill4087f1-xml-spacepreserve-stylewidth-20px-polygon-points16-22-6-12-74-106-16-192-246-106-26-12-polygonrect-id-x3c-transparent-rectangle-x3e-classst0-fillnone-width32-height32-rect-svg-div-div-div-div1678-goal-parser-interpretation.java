0a64a4ececf0bdad9f1637ce55a58416fcacb638
Shreyas P <72003665+shreyas0924@users.noreply.github.com>
2023-09-18 17:24:51 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (90.32%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-hidden-flex-wrap-wrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondiv-styletransform-rotate180deg-svg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1678-goal-parser-interpretation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-hidden-flex-wrap-wrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondiv-styletransform-rotate180deg-svg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1678-goal-parser-interpretation.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder str = new StringBuilder();
+        
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i) == 'G') str.append("G");
+            if(command.charAt(i)=='('){
+			    if(command.charAt(i+1)==')') {
+                    str.append('o'); i++;
+                }
+			    else{
+                    str.append("al"); i = i + 3;
+                }
+		    }
+        }
+        return str.toString();
+    }
+}
